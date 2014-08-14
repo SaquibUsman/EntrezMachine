93e5f7b889c1a9c9a22f468c35d69fbd18230dea
Saquib Usman <AtraxiaAir@LarryTheMacBookAir-4.local>
2014-08-14 18:06:05 -0400

    adding Unifishised object

---
+++ EntrezMachine/UnfinishedObject.h
@@ -0,0 +1,13 @@
+//
+//  UnfinishedObject.h
+//  EntrezMachine
+//
+//  Created by Saquib Usman on 8/14/14.
+//  Copyright (c) 2014 Saquib Usman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnfinishedObject : NSObject
+
+@end
---
+++ EntrezMachine/UnfinishedObject.m
@@ -0,0 +1,13 @@
+//
+//  UnfinishedObject.m
+//  EntrezMachine
+//
+//  Created by Saquib Usman on 8/14/14.
+//  Copyright (c) 2014 Saquib Usman. All rights reserved.
+//
+
+#import "UnfinishedObject.h"
+
+@implementation UnfinishedObject
+
+@end
